e19bbe275bc1fa30be7b5e0d039b2113da4e897f
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2015-01-29 21:31:23 +0000

    Set up tasks for clean, copy, minification and usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,137 @@
 module.exports = function (grunt) {
 // load all grunt tasks matching the `grunt-*` pattern
-    require('load-grunt-tasks')(grunt);
-    
+		require('load-grunt-tasks')(grunt);
+
 		grunt.initConfig({
-			//TODO: Run grunt-bower-install-simple to ensure all dependencies are up to date (OR manually run bower update in command line)
+			//clean main compiled css file and minified version.
+  		clean: {
+    		css: ['app/css/vendor', 'dist/css'],
+    		js: ['app/js/vendor', 'dist/js']
+			},
+			//Run grunt-bower-install-simple to ensure all dependencies are up to date (OR manually run bower update in command line)
+			"bower-install-simple": {
+				options: {
+					color: true,
+					directory: "bower_components"
+				}
+			},
 
 			//Compile Foundation SCSS into CSS
 			sass: {
-    		dist: {
-      		files: {
-        		'bower_components/foundation/css/foundation.css': 'bower_components/foundation/scss/app.scss',
-        		'app/css/styles.css': 'app/scss/app.scss',
-      		},
-      	}
-  		},
+				dist: {
+					files: {
+						'bower_components/foundation/css/foundation.css': 'bower_components/foundation/scss/app.scss',
+						'app/css/styles.css': 'app/scss/app.scss',
+					},
+				}
+			},
 			
 			//Copy specific bower_components files that will be needed, into app folder:
-			// TODO: install and copy fontawesome.css and lt-ie-9 dependencies
-			
 			bowercopy: {
-  			options: {
-    			srcPrefix: 'bower_components/'
-  			},
-  		scripts: {
-    		files: {
-      		'jquery.js': 'jquery/dist/jquery.js',
-     		  'foundation.js': 'foundation/js/foundation.js',
-    		},
-    		options: {
-      		destPrefix: 'app/js/vendor'
-    		},
-    	},  		
-    	
-    	styles: {
-    		files: {
-    			'foundation.css': 'foundation/css/foundation.css',
-    		},
-    		options: {
-    			destPrefix: 'app/css/vendor'
-    		},
+				options: {
+					srcPrefix: 'bower_components/'
+				},
+				scripts: {
+					files: {
+						'jquery.js': 'jquery/dist/jquery.js',
+						'foundation.js': 'foundation/js/foundation.js',
+					},
+				options: {
+					destPrefix: 'app/js/vendor'
+				},
+			},
+			
+			styles: {
+				files: {
+					'foundation.css': 'foundation/css/foundation.css',
+				},
+				options: {
+					destPrefix: 'app/css/vendor'
+				},
+			}
+		},
+
+		//Use usemin to concat and minify css and js dependencies.
+		useminPrepare: {
+			html: 'app/*.html',
+			options: {
+				dest: 'dist/index.html',
+			}
+		},
+		
+		concat: {
+			//Concatenate css/vendor/foundation.css + styles.css into app.css.
+			css: {
+				src: ['app/css/vendor/*.css','app/css/*.css'],
+				dest: 'dist/css/app.css'
+			},
+			//Concatenate js/vendor/*.js files into app.js.
+			js: {
+				src: ['app/js/vendor/*.js'],
+				dest: 'dist/js/app.js'
+			}
+		},
+		
+		//Minify css in dist/css folder
+		cssmin: {
+  		target: {
+    		files: [{
+    		expand: true,
+      		cwd: 'dist/css',
+      		src: ['*.css', '!*.min.css'],
+      		dest: 'dist/css',
+      		ext: '.min.css'
+    		}]
   		}
 		},
-			
-		//Use usemin to concat and minify css and js dependencies
 		
-		//TODO: Concatenate js/vendor/*.js files into app.js
-		//TODO: Concatenate css/vendor/foundation.css + styles.css into app.css
-			  		
-  });
-  
-  //TODO: grunt develop should be clean, sass, watch, autoprefixer, (bowercopy?)
-  //TODO: grunt bowercopy should be clean, (+ ideally bower install), bowercopy
-  //TODO: grunt build should be clean, (+ ideally bower install), sass, autoprefixer, bowercopy, usemin
-  
-  //TODO: add in CSSlint, image optimisations
-  
-  //TODO: wiredep will inject dependencies into HTML, then run bowercopy to copy files into /app folder
-  
+		//Minify js in dist/js folder
+		uglify: {
+    	my_target: {
+      	files: [{
+      		expand: true,
+      		cwd: 'dist/js',
+      		src: ['*.js', '!*.min.js'],
+      		dest: 'dist/js',
+      		ext: '.min.js'
+      	}]
+      }
+    },
+
+		usemin: {
+			html: 'dist/index.html',
+			css: 'dist/css/app.css',
+			js: 'dist/js/app.js'
+		},
+
+		copy:{
+			html: {
+				src: 'app/index.html',
+				dest: 'dist/index.html'
+			}
+		}
+	});
+
+	//TODO: grunt develop should be clean, bowercopy, sass, watch, autoprefixer
+	//TODO: grunt bowercopy should be clean, (+ ideally bower install), bowercopy
+	//TODO: grunt build should be clean, (+ ideally bower install), sass, autoprefixer, bowercopy, usemin
+	
+	//TODO: install and copy fontawesome.css and lt-ie-9 dependencies
+	//TODO: add in CSSlint
+	//TODO: add image optimisations
+	//TODO: add notifications
+	//TODO: add Autoprefixer
+	//TODO: make config dynamic
+	//TODO: add LiveReload
+
+	grunt.registerTask('cleanit', ['clean'])
 	grunt.registerTask('develop', ['sass','bowercopy']);
+	grunt.registerTask('bower-install', ['bower-install-simple']);
+	grunt.registerTask('bower-copy', ['bowercopy']);
+	grunt.registerTask('copyToDist', ['copy']);
+	grunt.registerTask('concatcss', ['concat:css']);
+	grunt.registerTask('concatjs', ['concat:js']);
+	grunt.registerTask('minifycss', ['cssmin']);
+	grunt.registerTask('minifyjs', ['uglify']);
+	grunt.registerTask('useMin', ['useminPrepare','usemin']);
 };
