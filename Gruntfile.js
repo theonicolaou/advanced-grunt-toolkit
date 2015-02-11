8ee36bf3b79096883af6d20572354b8cafe3ba02
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2015-02-11 20:29:54 +0000

    Indentation cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,309 +1,309 @@
 module.exports = function (grunt) {
 // load all grunt tasks matching the `grunt-*` pattern
-		require('load-grunt-tasks')(grunt);
+require('load-grunt-tasks')(grunt);
 
-		grunt.initConfig({
-			//clean main compiled css file and minified version.
-  		clean: {
-    		css: ['app/css/vendor', 'dist/css'],
-    		js: ['app/js/vendor', 'dist/js'],
-    		html: ['dist/*.html']
-			},
-			
-			//Run grunt-bower-install-simple to ensure all dependencies are up to date (OR manually run bower update in command line)
-			"bower-install-simple": {
-				options: {
-					color: true,
-					directory: "bower_components"
-				}
-			},
+grunt.initConfig({
+	//clean main compiled css file and minified version.
+	clean: {
+		css: ['app/css/vendor', 'dist/css'],
+		js: ['app/js/vendor', 'dist/js'],
+		html: ['dist/*.html']
+	},
+	
+	//Run grunt-bower-install-simple to ensure all dependencies are up to date (OR manually run bower update in command line)
+	"bower-install-simple": {
+		options: {
+			color: true,
+			directory: "bower_components"
+		}
+	},
 
-			//Compile Foundation SCSS and custom styles SCSS files into CSS
-			sass: {
-				dist: {
-					files: {
-						'bower_components/foundation/css/foundation.css': 'bower_components/foundation/scss/app.scss',
-						'app/css/styles.css': 'app/scss/app.scss',
-					},
-				}
+	//Compile Foundation SCSS and custom styles SCSS files into CSS
+	sass: {
+		dist: {
+			files: {
+				'bower_components/foundation/css/foundation.css': 'bower_components/foundation/scss/app.scss',
+				'app/css/styles.css': 'app/scss/app.scss',
 			},
+		}
+	},
 			
-			//Copy specific bower_components files that will be needed, into app folder:
-			bowercopy: {
-				options: {
-					srcPrefix: 'bower_components/'
-				},
-				scripts: {
-					files: {
-						'jquery.js': 'jquery/dist/jquery.js',
-						'foundation.js': 'foundation/js/foundation.js',
-					},
-				options: {
-					destPrefix: 'app/js/vendor'
-				},
-			},
-			
-			styles: {
-				files: {
-					'foundation.css': 'foundation/css/foundation.css',
-					'font-awesome.css': 'fontawesome/css/font-awesome.css',
-				},
-				options: {
-					destPrefix: 'app/css/vendor'
-				},
-			},
-
-			fonts: {
-				files: {
-					'/fonts': '/fontawesome/fonts/',
-				},
-				options: {
-					destPrefix: 'app/'
-				},
-			}
+	//Copy specific bower_components files that will be needed, into app folder:
+	bowercopy: {
+		options: {
+			srcPrefix: 'bower_components/'
 		},
-
-		//Use usemin to concat and minify css and js dependencies.
-		useminPrepare: {
-			html: 'app/*.html',
+		scripts: {
+			files: {
+				'jquery.js': 'jquery/dist/jquery.js',
+				'foundation.js': 'foundation/js/foundation.js',
+			},
 			options: {
-				dest: 'dist/index.html',
-			}
-		},
-		
-		concat: {
-			//Concatenate css/vendor/foundation.css + styles.css into app.css.
-			css: {
-				src: ['app/css/vendor/*.css','app/css/*.css'],
-				dest: 'dist/css/app.css'
+				destPrefix: 'app/js/vendor'
 			},
-			//Concatenate js/vendor/*.js files into app.js.
-			js: {
-				src: ['app/js/vendor/*.js'],
-				dest: 'dist/js/app.js'
+		},
+
+		styles: {
+			files: {
+				'foundation.css': 'foundation/css/foundation.css',
+				'font-awesome.css': 'fontawesome/css/font-awesome.css',
+			},
+			options: {
+				destPrefix: 'app/css/vendor'
+			},
+		},
+
+		fonts: {
+			files: {
+				'/fonts': '/fontawesome/fonts/',
+			},
+			options: {
+				destPrefix: 'app/'
+			},
+		}
+	},
+
+	//Use usemin to concat and minify css and js dependencies.
+	useminPrepare: {
+		html: 'app/*.html',
+		options: {
+			dest: 'dist/index.html',
+		}
+	},
+		
+	concat: {
+		//Concatenate css/vendor/foundation.css + styles.css into app.css.
+		css: {
+			src: ['app/css/vendor/*.css','app/css/*.css'],
+			dest: 'dist/css/app.css'
+		},
+		//Concatenate js/vendor/*.js files into app.js.
+		js: {
+			src: ['app/js/vendor/*.js'],
+			dest: 'dist/js/app.js'
+		}
+	},
+		
+	//Minify css in dist/css folder
+	cssmin: {
+		target: {
+			files: [{
+				expand: true,
+				cwd: 'dist/css',
+				src: ['*.css', '!*.min.css'],
+				dest: 'dist/css',
+				ext: '.min.css'
+			}]
+		}
+	},
+		
+	//Minify js in dist/js folder
+	uglify: {
+		my_target: {
+			files: [{
+				expand: true,
+				cwd: 'dist/js',
+				src: ['*.js', '!*.min.js'],
+				dest: 'dist/js',
+				ext: '.min.js'
+			}]
+		}
+	},
+
+	usemin: {
+		html: 'dist/index.html',
+		css: 'dist/css/app.css',
+		js: 'dist/js/app.js'
+	},
+
+	copy:{
+		html: {
+			src: 'app/index.html',
+			dest: 'dist/index.html'
+		}
+	},
+
+	//lint main custom CSS file.
+	csslint: {
+		strict: {
+			src: ['app/css/styles.css']
+		}
+	},
+	
+	autoprefixer: {
+		options: {
+			browsers: ['last 3 versions', 'ie 9', 'ie 8']
+		},
+		
+		dist: {
+			files: {
+				'app/css/styles.css': 'app/css/styles.css'
+			}
+		}
+	},
+
+	//watch for changes to SCSS files.
+	watch: {
+		sass: {
+			files: ['app/scss/*.scss'],
+			tasks: ['sass'],
+		},
+
+		html: {
+			files: ['app/*.html'],
+			options: {
+				livereload: 8888
 			}
 		},
-		
-		//Minify css in dist/css folder
-		cssmin: {
-  		target: {
-    		files: [{
-    		expand: true,
-      		cwd: 'dist/css',
-      		src: ['*.css', '!*.min.css'],
-      		dest: 'dist/css',
-      		ext: '.min.css'
-    		}]
-  		}
+
+		styles: {
+			files: ['app/css/styles.css'],
+			tasks: ['autoprefixer']
 		},
-		
-		//Minify js in dist/js folder
+
+		options: {
+			livereload: true
+		}
+	},
+
+	connect: {
+		dev: {
+			options: {
+				port: 8888,
+				hostname: 'localhost',
+				livereload: true,
+				open: true
+				base: 'app/'
+			},
+
+			livereload: {
+				options: {
+					port: 8888
+				}
+			}
+		},
+
+		dist: {
+			options: {
+				port: 9000,
+				hostname: 'localhost',
+				open: true,
+				base: 'dist/'
+			}
+		}
+	},
+
+	//configurations for OS X notifications, for each task.
+	notify: {
+		clean: {
+			options: {
+				title: "Clean",
+				message: "Files cleaned",
+				success: true,
+				duration: 5	
+			}
+		},
+
+		"bower-install-simple": {
+			options: {
+				title: "Install Bower dependencies",
+				message: "Bower dependencies installed/updated",
+				success: true,
+				duration: 5	
+			}
+		},
+
+		bowercopy: {
+			options: {
+				title: "Copy Bower dependencies",
+				message: "Bower dependencies copied",
+				success: true,
+				duration: 5	
+			}
+		},
+
+		sass: {
+			options: {
+				title: "SCSS Compilation",
+				message: "SCSS compilation complete",
+				success: true,
+				duration: 5	
+			}
+		},
+
+		copy: {
+			options: {
+				title: "Copy files to Dist",
+				message: "Files copied to /dist folder",
+				success: true,
+				duration: 5	
+			}
+		},
+
+		concatcss: {
+			options: {
+				title: "CSS file concatenation",
+				message: "CSS files concatenated",
+				success: true,
+				duration: 5
+			}
+		},
+
+		concatjs: {
+			options: {
+				title: "JS file concatenation",
+				message: "JS files concatenated",
+				success: true,
+				duration: 5
+			}
+		},
+
+		cssmin: {
+			options: {
+				title: "CSS file minification",
+				message: "CSS files minified",
+				success: true,
+				duration: 5
+			}
+		},
+
 		uglify: {
-    	my_target: {
-      	files: [{
-      		expand: true,
-      		cwd: 'dist/js',
-      		src: ['*.js', '!*.min.js'],
-      		dest: 'dist/js',
-      		ext: '.min.js'
-      	}]
-      }
-    },
+			options: {
+				title: "JS file minification",
+				message: "JS files minified",
+				success: true,
+				duration: 5
+			}
+		},
 
 		usemin: {
-			html: 'dist/index.html',
-			css: 'dist/css/app.css',
-			js: 'dist/js/app.js'
-		},
-
-		copy:{
-			html: {
-				src: 'app/index.html',
-				dest: 'dist/index.html'
+			options: {
+				title: "usemin tasks",
+				message: "File paths updated with usemin",
+				success: true,
+				duration: 5
 			}
 		},
 
-		//lint main custom CSS file.
 		csslint: {
-			strict: {
-				src: ['app/css/styles.css']
+			options: {
+				title: "CSS Lint",
+				message: "CSS lint complete",
+				success: true,
+				duration: 5
 			}
 		},
-		
+
 		autoprefixer: {
 			options: {
-				browsers: ['last 3 versions', 'ie 9', 'ie 8']
-			},
-			
-			dist: {
-				files: {
-					'app/css/styles.css': 'app/css/styles.css'
-  			}
-      }
-    },
-  	
-  	//watch for changes to SCSS files.
-  	watch: {
-    	sass: {
-      	files: ['app/scss/*.scss'],
-      	tasks: ['sass'],
-    	},
-    	
-    	html: {
-    		files: ['app/*.html'],
-    		  options: {
-      			livereload: 8888
-      		}
-    		},
-    	
-      styles: {
-        files: ['app/css/styles.css'],
-        tasks: ['autoprefixer']
-      },
-      
-			options: {
-  			livereload: true
- 			}
-    },
-    
-    connect: {
-    	dev: {
-    		options: {
-    			port: 8888,
-    			hostname: 'localhost',
-    			livereload: true,
-    			open: true
-    			base: 'app/'
-    		},
-    		
-    		livereload: {
-    			options: {
-    				port: 8888
-    			}
-    		}
-    	},
-      
-      dist: {
-      	options: {
-        	port: 9000,
-        	hostname: 'localhost',
-        	open: true,
-        	base: 'dist/'
-      	}
-    	}
-  	},
-		
-		//configurations for OS X notifications, for each task.
-  	notify: {
-  		clean: {
-  			options: {
-  				title: "Clean",
-      		message: "Files cleaned",
-      		success: true,
-      		duration: 5	
-  			}
-  		},
-  		
-  		"bower-install-simple": {
-  			options: {
-  				title: "Install Bower dependencies",
-      		message: "Bower dependencies installed/updated",
-      		success: true,
-      		duration: 5	
-  			}
-  		},
-  		
-  		bowercopy: {
-  			options: {
-  				title: "Copy Bower dependencies",
-      		message: "Bower dependencies copied",
-      		success: true,
-      		duration: 5	
-  			}
-  		},
-  			
-  		sass: {
-  			options: {
-  				title: "SCSS Compilation",
-      		message: "SCSS compilation complete",
-      		success: true,
-      		duration: 5	
-  			}
-  		},
-  		
-  		copy: {
-  			options: {
-  				title: "Copy files to Dist",
-      		message: "Files copied to /dist folder",
-      		success: true,
-      		duration: 5	
-  			}
-  		},
-  			
-  		concatcss: {
-  			options: {
-  				title: "CSS file concatenation",
-      		message: "CSS files concatenated",
-      		success: true,
-      		duration: 5
-  			}
-  		},
-  		
-  		concatjs: {
-  			options: {
-  				title: "JS file concatenation",
-      		message: "JS files concatenated",
-      		success: true,
-      		duration: 5
-  			}
-  		},
-  			
-  		cssmin: {
-  			options: {
-  				title: "CSS file minification",
-      		message: "CSS files minified",
-      		success: true,
-      		duration: 5
-  			}
-  		},
-  		
-  		uglify: {
-  			options: {
-  				title: "JS file minification",
-      		message: "JS files minified",
-      		success: true,
-      		duration: 5
-  			}
-  		},
-  		
-  		usemin: {
-  			options: {
-      		title: "usemin tasks",
-      		message: "File paths updated with usemin",
-      		success: true,
-      		duration: 5
-    		}
-  		},
-  			
-  		csslint: {
-  			options: {
-      		title: "CSS Lint",
-      		message: "CSS lint complete",
-      		success: true,
-      		duration: 5
-    		}
-  		},
-  		 	
-  		autoprefixer: {
-  			options: {
-  				title: "Autoprefixer",
-  				message: "Vendor prefixes added to CSS files",
-  				success: true,
-  				duration: 5	
-  			}
-  		},
-  	},
-	});
+				title: "Autoprefixer",
+				message: "Vendor prefixes added to CSS files",
+				success: true,
+				duration: 5	
+			}
+		},
+	},
+});
 
 	//TODO: grunt develop should be clean, bowercopy, sass, watch, autoprefixer
 	//TODO: grunt bowercopy should be clean, (+ ideally bower install), bowercopy
@@ -317,7 +317,9 @@ module.exports = function (grunt) {
 
 	grunt.registerTask('cleanit', ['clean', 'notify:clean']);
 	grunt.registerTask('develop', ['sass','notify:sass','watch']);
-	grunt.registerTask('build', ['clean', 'notify:clean','bower-install-simple','notify:bower-install-simple','bowercopy','notify:bowercopy','copy','autoprefixer','notify:autoprefixer','concat:css','notify:concatcss','concat:js','notify:concatjs','cssmin','notify:cssmin','uglify','notify:uglify','useminPrepare','usemin','notify:usemin','connect:dist']);
+	grunt.registerTask('build', ['clean', 'notify:clean','bower-install-simple','notify:bower-install-simple','bowercopy','notify:bowercopy',
+		'copy','autoprefixer','notify:autoprefixer','concat:css','notify:concatcss','concat:js','notify:concatjs','cssmin','notify:cssmin',
+		'uglify','notify:uglify',		'useminPrepare','usemin','notify:usemin','connect:dist']);
 	grunt.registerTask('prefixcss', ['autoprefixer','notify:autoprefixer']);
 	grunt.registerTask('lintcss', ['csslint','notify:csslint']);
 	grunt.registerTask('serve', ['connect:dev']);
